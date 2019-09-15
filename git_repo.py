1faeb2e0d076a3c04a195c3616cccfd01e32c8f8
reidbe <37275718+reidbe@users.noreply.github.com>
2019-09-15 21:42:58 +0200

    Update git_repo.py

--- git_repo.py
+++ git_repo.py
@@ -2,18 +2,18 @@ from github import Github
 g = Github("rei.berge@gmail.com", "snObbA2019")
 org = g.get_organization('RB-KML-database')
 repo = org.get_repo("test_filer")
-def save_file(filename, forklaring, filename_git = 0 ): #Lagrer ny fil til github
+def save_file(filename, forklaring, filename_git = 0 ): 
   if  filename_git == 0: filename_git = filename
   with open(filename, 'r') as myfile:
     tekst_fil = myfile.read()
   repo.create_file(filename_git, forklaring, tekst_fil)
 
 
-def save_string_file(tekst_fil, forklaring, filename_git):  #Lagrer streng som fil til github
+def save_string_file(tekst_fil, forklaring, filename_git):  
   repo.create_file(filename_git + '.txt', forklaring, tekst_fil)
 
 
-def update_file(filename, forklaring, filename_git = 0 ):#oppdaterer fil som finnes på github
+def update_file(filename, forklaring, filename_git = 0 ):
   if  filename_git == 0: filename_git = filename
   contents = repo.get_contents(filename_git)
   with open(filename, 'r') as myfile:
