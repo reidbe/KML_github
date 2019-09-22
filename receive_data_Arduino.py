7986309b16bf6e3371001dda82aa2444a105cc8a
reidbe <37275718+reidbe@users.noreply.github.com>
2019-09-22 16:07:50 +0200

    Update receive_data_Arduino.py

--- receive_data_Arduino.py
+++ receive_data_Arduino.py
@@ -1,14 +1,28 @@
 import serial_stuff
-import save_to_txt
 import time
 tid_stop = 7
+
+def create_file(filename):
+    save_measure_file = open(filename + '.txt', 'w')
+    save_measure_file.close()
+
+
+def save_to_txt(string, filename):
+    save_measure_file = open(filename + '.txt', 'a')
+    save_measure_file.write(string)
+    save_measure_file.close()
+
+
+
+
+
 
 feather_data = serial_stuff.receive_data_from_Arduino()
 while 1:
     feather_data.update()
     if (feather_data.new_data == True):
         filename = time.strftime('%Y%m%d-%H%M%S') 
-        save_to_txt.create_file(filename)
+        create_file(filename)
         tid = time.time()
 
         while 1:
@@ -19,7 +33,7 @@ while 1:
                 try:
                     string = feather_data.string_received[:feather_data.string_received.index('\r')]
                     print(string)
-                    save_to_txt.save_to_txt(string+'\n', filename)
+                    save_to_txt(string+'\n', filename)
                 except:
                     a = 12              
                 tid = time.time()
